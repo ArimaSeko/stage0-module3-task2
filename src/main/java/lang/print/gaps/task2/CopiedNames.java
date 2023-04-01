7e0ac876e665ac51ef6c983fc10d3397a72d3c8e
ArimaSeko <120338228+ArimaSeko@users.noreply.github.com>
2023-04-01 23:11:55 +0600

    Update CopiedNames.java

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,17 +2,17 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-    int variable = 0;
-    int variableOne = 2000;
-    int value = 1000;
-    int variableTwo = 4000;
-    int valueTwo = 3000;
-    int variables = 5000;
-    int variablesOne = 7000;
-    int valueOne = 90000;
-    int million = 100000;
-    int billion = 1000000;
-    int oneForSeven = 1234567;
-    int myNumber = 1234567;
+    int n = 0;
+    int i = 2000;
+    int  j= 1000;
+    int k = 4000;
+    int a = 3000;
+    int b = 5000;
+    int c = 7000;
+    int d = 90000;
+    int l = 100000;
+    int q = 1000000;
+    int w = 1234567;
+    int e = 1234567;
     }
 }
